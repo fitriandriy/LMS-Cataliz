008d1bc85f9e1064f6e9c7c5c6091cb97ba72cdf
mhazrla <mhazrla@gmail.com>
2023-04-16 23:25:10 +0700

    solving bug on db connection (only in hilmy's workspace)

--- back-end/src/modules/course/router.ts
+++ back-end/src/modules/course/router.ts
@@ -3,10 +3,9 @@ import * as controller from "./controller/index.js";
 import { authMiddleware } from "@src/middleware/auth.middleware.js";
 
 const router = Router();
-router.use(authMiddleware);
 router.get("/", controller.retrieveAllController);
-router.post("/", authMiddleware, controller.createController);
 router.get("/:id", controller.retrieveController);
+router.post("/", authMiddleware, controller.createController);
 router.patch("/:id", authMiddleware, controller.updateController);
 router.delete("/:id", authMiddleware, controller.deleteController);
 
